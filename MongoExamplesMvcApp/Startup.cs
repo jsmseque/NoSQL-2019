61c6af46f8cd27b244dc71d5e49c2f31bbd53b47
Laboratorios <Laboratorios@V13>
2019-11-21 20:29:45 -0600

    mvc

---
+++ MongoExamplesMvcApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MongoExamplesMvcApp.Startup))]
+namespace MongoExamplesMvcApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
